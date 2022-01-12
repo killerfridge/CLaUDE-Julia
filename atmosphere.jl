dc4f9531e17181c4ab6cdbc4232bea6c2b33449a
danielrussell-brain <daniel.rb@hotmail.co.uk>
2022-01-12 11:11:46 +0000

    created first draft of CLauDE

---
+++ atmosphere.jl
@@ -0,0 +1,85 @@
+module Atmos_Mod
+include("constants.jl")
+using .Constants
+using PyCall
+
+interpolate = pyimport("scipy.interpolate")
+
+export Pixel, update_temp!, update_velocity!, advect!, interpolate, push!
+
+mutable struct Pixel
+    lat::Float64
+    lon::Float64
+    temp::Float64
+    velocity_east_west::Float64
+    velocity_north_south::Float64
+    coriolis::Float64
+    function Pixel(lat::Float64, lon::Float64)
+        coriolis::Float64 = cos(lat) * 1e-5
+        temp::Float64 = 270 + (20 * sin(lat))
+        return new(lat, lon, temp, 0., 0., coriolis)
+    end
+end
+
+
+mutable struct Atmosphere
+    pixels::Vector{Pixel}
+    function Atmosphere()
+        return new([])
+    end
+end
+
+
+function push!(s::Atmosphere, x::Pixel)
+    push!(s.pixels, x)
+end
+
+
+function update_temp!(pixel::Pixel, constants::Constant=constants, sun_lon::Float64=0.)
+
+    pixel.temp = constants.dt * (
+        constants.solar *
+        (1 - constants.albedo) *
+        max(0, sin(pixel.lat)) * 
+        max(0, sin(lon - sun_lon)) - 
+        (constants.boltzman) * pixel.temp^4
+    ) / constants.heatcapacity
+
+    return pixel
+
+end
+
+function update_velocity!(pixel::Pixel, constants::Constant=constants)
+    # TODO: recreate the field_d_lat/field_d_lon functions from the original (0s as placeholders)
+    pixel.velocity_east_west -= constants.dt * (
+        pixel.velocity_east_west * 0 + 
+        pixel.velocity_north_south * 0 +
+        pixel.coriolis * pixel.velocity_north_south + 0
+    )
+    # TODO: recreate the field_d_lat/field_d_lon functions from the original (0s as placeholders)
+    pixel.velocity_north_south -= constants.dt * (
+        pixel.velocity_east_west * 0 + 
+        pixel.velocity_north_south * 0 +
+        pixel.coriolis * pixel.velocity_east_west + 0
+    )
+
+    return pixel
+end
+
+function advect!(pixel::Pixel, constants::Constant=constants)
+
+    # TODO: recreate the field_d_lat/field_d_lon functions from the original
+
+    pixel.temp -= constants.dt * (
+        pixel.temp * 0 + 
+        pixel.velocity_east_west * 0 + 
+        pixel.temp * 0 +
+        pixel.velocity_north_south * 0
+    )
+
+    return pixel
+
+end
+
+
+end
---
+++ constants.jl
@@ -0,0 +1,39 @@
+module Constants
+
+export constants, Constant, config
+
+struct Constant
+    solar::Int16
+    heatcapacity::Int32
+    albedo::Float32
+    radius::Int32
+    dt::Int32
+    day::Int32
+    year::Float32
+    boltzman::Float64
+
+    function Constant()
+        return new(
+            1370,
+            1e5, 
+            0.3, 
+            6.4e6,
+            18*60, 
+            60 * 60 * 24,
+            365.25 * 60 * 60 * 24,
+            5.67e-8
+            )
+    end
+end
+
+struct Config
+    res::Int32
+    function Config()
+        return new(75)
+    end
+end
+
+constants = Constant()
+config = Config()
+
+end
